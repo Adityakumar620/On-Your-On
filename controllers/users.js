f473518eee168747e43ed8d20320672bb4879a90
Aditya kumar <arajsg8226@gmail.com>
2025-03-09 00:48:38 +0530

    Update some file

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,7 @@ module.exports.signup = async(req,res)=>{
             if(err){
                 return next(err);
             }
-            req.flash("success" , "Welcome to Wanderlust!");
+            req.flash("success" , "Welcome to On Your Own!");
             res.redirect("/listings");
         });
     }catch(err){
@@ -29,7 +29,7 @@ module.exports.renderLoginForm = (req,res)=>{
 };
 
 module.exports.login = async(req,res)=>{
-    req.flash("success","Welcome back to Wonderlust!");
+    req.flash("success","Welcome back to On Your Own!");
     let redirectUrl = res.locals.redirectUrl || "/listings";
     res.redirect(redirectUrl);
 };
